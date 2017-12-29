ef00a7fda9495108c40c3a351c8593d8a0b41c74
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2017-12-29 14:12:09 -0800

    Add sizes of dataset and helper vars

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ from sklearn.metrics import confusion_matrix
 
 from tensorflow.examples.tutorials.mnist import input_data
 data = input_data.read_data_sets("data/MNIST", one_hot=True)
+
+print("Size of training set:", len(data.train.labels))
+print("Size of test set:", len(data.test.labels))
+print("Size of validation set:", len(data.validation.labels))
+
+img_size = 28
+img_size_flat = img_size * img_size
+img_shape = (img_size, img_size)
+num_classes = 10
