16c405d97f836d36a6d117dfc6629c3ce56911b9
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2017-12-29 14:21:20 -0800

    Add helper function

--- main.py
+++ main.py
@@ -14,3 +14,22 @@ img_size = 28
 img_size_flat = img_size * img_size
 img_shape = (img_size, img_size)
 num_classes = 10
+
+data.test.cls = np.array(label.argmax() for label in data.test.labels)
+
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+        ax.set_xlabel(xlabel)
+        ax.set_xticks([])
+        ax.set_yticks([])
+    plt.show()
+
+images = data.test.images[0:9]
