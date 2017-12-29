f59358eca994c3862b470572ae15ae03c221dfdb
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2017-12-29 15:07:43 -0800

    Got rid of junk

--- main.py
+++ main.py
@@ -1,35 +1,102 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-from sklearn.metrics import confusion_matrix
 
 from tensorflow.examples.tutorials.mnist import input_data
-data = input_data.read_data_sets("data/MNIST", one_hot=True)
+data = input_data.read_data_sets("data/MNIST/", one_hot=True)
 
-print("Size of training set:", len(data.train.labels))
-print("Size of test set:", len(data.test.labels))
-print("Size of validation set:", len(data.validation.labels))
+print("Size of:")
+print("Training set:\t\t{}".format(len(data.train.labels)))
+print("Test set:\t\t{}".format(len(data.test.labels)))
+print("Validation set:\t\t{}".format(len(data.validation.labels)))
 
 img_size = 28
 img_size_flat = img_size * img_size
 img_shape = (img_size, img_size)
 num_classes = 10
 
-data.test.cls = np.array(label.argmax() for label in data.test.labels)
+data.test.cls = np.array([label.argmax() for label in data.test.labels])
 
-def plot_images(images, cls_true, cls_pred=None):
-    assert len(images) == len(cls_true) == 9
-    fig, axes = plt.subplots(3, 3)
+x = tf.placeholder(tf.float32, [None, img_size_flat])
+y_true = tf.placeholder(tf.float32, [None, num_classes])
+y_true_cls = tf.placeholder(tf.int64, [None])
+
+weights = tf.Variable(tf.zeros([img_size_flat, num_classes]))
+biases = tf.Variable(tf.zeros([num_classes]))
+
+logits = tf.matmul(x, weights) + biases
+y_pred = tf.nn.softmax(logits)
+y_pred_cls = tf.argmax(y_pred, axis=1)
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cost)
+
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+batch_size = 100
+
+def optimize(num_iterations):
+    for i in range(num_iterations):
+        x_batch, y_true_batch = data.train.next_batch(batch_size)
+        feed_dict_train = {x: x_batch, y_true: y_true_batch}
+        session.run(optimizer, feed_dict=feed_dict_train)
+
+feed_dict_test = {x: data.test.images,
+                  y_true: data.test.labels,
+                  y_true_cls: data.test.cls}
+
+def print_accuracy():
+    acc = session.run(accuracy, feed_dict=feed_dict_test)
+    print("Accuracy on test-set: {0:.1%}".format(acc))
+
+def plot_weights():
+    w = session.run(weights)
+    w_min = np.min(w)
+    w_max = np.max(w)
+    fig, axes = plt.subplots(3, 4)
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
     for i, ax in enumerate(axes.flat):
-        ax.imshow(images[i].reshape(img_shape), cmap='binary')
-        if cls_pred is None:
-            xlabel = "True: {0}".format(cls_true[i])
-        else:
-            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
-        ax.set_xlabel(xlabel)
+        if i<10:
+            image = w[:, i].reshape(img_shape)
+            ax.set_xlabel("Weights: {0}".format(i))
+            ax.imshow(image, vmin=w_min, vmax=w_max, cmap='seismic')
         ax.set_xticks([])
         ax.set_yticks([])
     plt.show()
 
-images = data.test.images[0:9]
+print("Without any training whatsoever:")
+print_accuracy()
+plot_weights()
+
+print("With one training iteration:")
+optimize(num_iterations=1)
+print_accuracy()
+plot_weights()
+
+print("With 10 training iterations:")
+optimize(num_iterations=9)
+print_accuracy()
+plot_weights()
+
+print("With 100 training iterations:")
+optimize(num_iterations=90)
+print_accuracy()
+plot_weights()
+
+print("With 1,000 training iterations:")
+optimize(num_iterations=900)
+print_accuracy()
+plot_weights()
+
+print("With 10,000 training iterations:")
+optimize(num_iterations=9000)
+print_accuracy()
+plot_weights()
+
+session.close()
