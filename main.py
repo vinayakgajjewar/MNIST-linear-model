e8e385d8b2b768f48ff71495a33eb770a98555a2
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2017-12-29 14:00:10 -0800

    Collect and extract data

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 from sklearn.metrics import confusion_matrix
+
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data.read_data_sets("data/MNIST", one_hot=True)
